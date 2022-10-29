61465fa204fbb28914880d5c8eef78c059638906
Anton Zaharia <zaharia_anton@yahoo.com>
2022-10-29 13:18:27 +0100

    navbar re-styled

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,7 @@
 import '@hotwired/turbo-rails'
 import './controllers'
 import 'flowbite'
+
+// Not sure if this is good or not for Turbo but it fixes the dropdown after redirect
+import { Turbo } from '@hotwired/turbo-rails'
+Turbo.session.drive = false
