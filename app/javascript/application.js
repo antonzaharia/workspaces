553dfb09fc888a82c4a2eabb6e2b6bd4bfd7fab4
Anton Zaharia <zaharia_anton@yahoo.com>
2022-11-01 13:57:53 +0000

    dropdown fixed

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,17 @@
 import '@hotwired/turbo-rails'
 import './controllers'
 import 'flowbite'
+import { Application } from '@hotwired/stimulus'
+
+const application = Application.start()
+
+// Import and register all TailwindCSS Components
+import { Alert, Autosave, Dropdown, Modal, Tabs, Popover, Toggle, Slideover } from 'tailwindcss-stimulus-components'
+application.register('alert', Alert)
+application.register('autosave', Autosave)
+application.register('dropdown', Dropdown)
+application.register('modal', Modal)
+application.register('tabs', Tabs)
+application.register('popover', Popover)
+application.register('toggle', Toggle)
+application.register('slideover', Slideover)
