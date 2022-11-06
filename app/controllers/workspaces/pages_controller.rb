a3188d26d402853f298fe89a390e83a1910c3f65
Anton Zaharia <zaharia_anton@yahoo.com>
2022-11-06 10:54:03 +0000

    small tweaks

--- app/controllers/workspaces/pages_controller.rb
+++ app/controllers/workspaces/pages_controller.rb
@@ -1,7 +1,7 @@
 module Workspaces
   class PagesController < WorkspacesController
     def show
-      @workspace_users = @workspace.workspace_users.all
+      @workspace_users = @workspace.workspace_users.all.order(created_at: :desc)
     end
   end
 end
