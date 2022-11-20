ccfa36eb79dfcdc22abdca81423075800e9b3c87
Anton Zaharia <zaharia_anton@yahoo.com>
2022-11-20 11:05:54 +0000

    invitation mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,4 +4,10 @@ class UserMailer < ApplicationMailer
     @url = root_url
     mail(to: @user.email, subject: 'Welcome to Gooff')
   end
+
+  def invitation_email
+    @workspace_user = params[:workspace_user]
+    @url = root_url
+    mail(to: @workspace_user.email, subject: "You've been invited to GOOFF!")
+  end
 end
